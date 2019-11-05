b4a70ef44ee62ccc9c18fbb9b2f9f85a397eaaff
Marco De La Pierre <marco.delapierre@gmail.com>
2019-11-05 16:52:09 +0800

    updated install-mpich script

--- files/install-mpich.sh
+++ files/install-mpich.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+USERID="ubuntu"
+
 MPICH_VERSION="3.1.4"
-MPICH_CONFIGURE_OPTIONS="--enable-fast=all,O3 --prefix=$(pwd)/mpich-${MPICH_VERSION}/apps"
+MPICH_ROOT="/opt/mpich"
+MPICH_DIR="$MPICH_ROOT/mpich-$MPICH_VERSION/apps"
+MPICH_CONFIGURE_OPTIONS="--enable-fast=all,O3 --prefix=$MPICH_DIR"
 MPICH_MAKE_OPTIONS="-j4"
 unset F90
+
+sudo mkdir -p $MPICH_ROOT
+sudo chown ${USERID}:${USERID} $MPICH_ROOT
+
+cd $MPICH_ROOT
 
 wget http://www.mpich.org/static/downloads/${MPICH_VERSION}/mpich-${MPICH_VERSION}.tar.gz
 tar xvzf mpich-${MPICH_VERSION}.tar.gz
@@ -12,5 +21,10 @@ cd mpich-${MPICH_VERSION}
 
 ./configure ${MPICH_CONFIGURE_OPTIONS}
 make ${MPICH_MAKE_OPTIONS}
-make install
+sudo make install
 
+echo "export PATH=\"$MPICH_DIR/bin:\$PATH\"" >> $(eval echo ~${USERID})/.bashrc
+echo "export CPATH=\"$MPICH_DIR/include:\$CPATH\"" >> $(eval echo ~${USERID})/.bashrc
+echo "export LD_LIBRARY_PATH=\"$MPICH_DIR/lib:\$LD_LIBRARY_PATH\"" >> $(eval echo ~${USERID})/.bashrc
+echo "export LIBRARY_PATH=\"$MPICH_DIR/lib:\$LIBRARY_PATH\"" >> $(eval echo ~${USERID})/.bashrc
+echo "export MANPATH=\"$MPICH_DIR/share/man:\$MANPATH\"" >> $(eval echo ~${USERID})/.bashrc
