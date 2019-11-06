bd53e626901faf817563f5f83f82266197ffe399
Marco De La Pierre <marco.delapierre@gmail.com>
2019-11-07 00:47:53 +0800

    07 openfoam mpi demo files

---
+++ demos/07_openfoam/mpi.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=mpi
+#SBATCH --ntasks=2
+#SBATCH --ntasks-per-node=2
+#SBATCH --time=00:20:00
+#SBATCH --export=NONE
+
+
+# pre-processing
+srun --export=all -n 1 \
+  singularity exec openfoam_v1812.sif \
+  blockMesh | tee log.blockMesh
+
+srun --export=all -n 1 \
+  singularity exec openfoam_v1812.sif \
+  topoSet | tee log.topoSet
+
+srun --export=all -n 1 \
+  singularity exec openfoam_v1812.sif \
+  decomposePar -fileHandler uncollated | tee log.decomposePar
+
+
+# run OpenFoam with MPI
+srun --export=all -n 2 \
+  singularity exec openfoam_v1812.sif \
+  simpleFoam -fileHandler uncollated -parallel | tee log.simpleFoam
+
+
+# post-processing
+srun --export=all -n 1 \
+  singularity exec openfoam_v1812.sif \
+  reconstructPar -latestTime -fileHandler uncollated | tee log.reconstructPar
+
---
+++ demos/07_openfoam/update-settings.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Gloabl settings
+aeg_numberOfSubdomains="2" # must equal $SLURM_NTASKS in the main run below
+aeg_simpleCoeffs="2 1 1" # product must equal numberOfSubdomains above
+# Applying global settings
+sed -i -e "s,^ *numberOfSubdomains.*,numberOfSubdomains  $aeg_numberOfSubdomains;," \
+       -e "/^ *simpleCoeffs/ { n; n; s,^ *n *(.*,    n           ($aeg_simpleCoeffs);,}" system/decomposeParDict
+
+# Settings for main run
+aeg_startFrom="startTime" # "latestTime" # for a restart
+aeg_startTime="0"
+aeg_endTime="20"
+aeg_writeInterval="5"
+aeg_purgeWrite="0"
+# Applying settings for main run
+sed -i -e "s,^ *startFrom.*,startFrom    $aeg_startFrom;," \
+       -e "s,^ *startTime.*,startTime    $aeg_startTime;," \
+       -e "s,^ *endTime.*,endTime    $aeg_endTime;," \
+       -e "s,^ *writeInterval.*,writeInterval    $aeg_writeInterval;," \
+       -e "s,^ *purgeWrite.*,purgeWrite    $aeg_purgeWrite;," system/controlDict
+
