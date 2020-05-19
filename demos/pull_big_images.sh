53e4bf185dfbec3799748a68496ebd3b5a7c5bb9
Marco De La Pierre <marco.delapierre@gmail.com>
2020-05-19 17:10:58 +0800

    updated pull script to remove SIFPATH

--- demos/pull_big_images.sh
+++ demos/pull_big_images.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-mkdir -p $SIFPATH
+# executing a dummy command just to cause images to be downloaded in the cache
 
-singularity pull --dir $SIFPATH docker://biocontainers/blast:v2.2.31_cv2
+# day 1
+singularity exec docker://biocontainers/blast:v2.2.31_cv2 echo ciao
 
-singularity pull --dir $SIFPATH docker://trinityrnaseq/trinityrnaseq:2.8.6
-singularity pull --dir $SIFPATH docker://nvcr.io/hpc/gromacs:2018.2
-singularity pull --dir $SIFPATH library://marcodelapierre/beta/openfoam:v1812
+# day 2
+singularity exec docker://rocker/tidyverse:3.6.1 echo ciao
+singularity exec docker://jupyter/datascience-notebook:latest echo ciao
+
+# day 3
+singularity exec library://marcodelapierre/beta/openfoam:v1812 echo ciao
+singularity exec docker://nvcr.io/hpc/gromacs:2018.2 echo ciao
+singularity exec docker://trinityrnaseq/trinityrnaseq:2.8.6 echo ciao
+
+# bonus
+singularity exec docker://nextflow/rnaseq-nf:latest echo ciao
