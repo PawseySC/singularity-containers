3ac75ba71b41c621eb7da714238f51ebe704498e
Marco De La Pierre <marco.delapierre@gmail.com>
2020-06-02 16:43:29 +0800

    updated script to install modules

--- files/install-modules.sh
+++ files/install-modules.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+export USERID=$USER
+
 sudo apt update
 sudo apt install -y environment-modules
 
 cd /usr/bin
-
 # assuming version 8.6 here
 sudo ln -s tclsh8.6 tclsh
+
+MODULE_VERSION=$( module -V | cut -d ' ' -f 3 )
+echo "export MODULE_VERSION=\"$MODULE_VERSION\"" >> $(eval echo ~${USERID})/.bashrc
 
