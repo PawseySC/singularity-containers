7c805001e4266c812cc10ee443462b1cf8c26337
Marco De La Pierre <marco.delapierre@gmail.com>
2020-06-05 15:31:19 +0800

    edits to singularity install script

--- files/install-singularity.sh
+++ files/install-singularity.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-export USERID=$USER
+# define custom variables
+GO_DIR="/opt"
+GO_VERSION="1.13.10"
 
+SING_DIR="/usr/local"
+SING_VERSION="v3.5.2" # or another tag or branch if you like
+
+USERID="$USER" # do not change this
+
+
+# install pre-requisites
 sudo apt-get update
 sudo apt-get install -y \
     build-essential \
@@ -13,24 +22,23 @@ sudo apt-get install -y \
     libseccomp-dev \
     pkg-config
 
+
 # install go
-export GO_DIR=/opt
-export GOPATH=${GO_DIR}/go
+GOPATH="${GO_DIR}/go"
 sudo mkdir -p $GOPATH
 sudo chown ${USERID}:${USERID} $GOPATH
 cd $GO_DIR
-export VERSION=1.13.10 OS=linux ARCH=amd64
-sudo wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
-sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
-export GOPATH=$(pwd)/go
-export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin
+OS="linux"
+ARCH="amd64"
+sudo wget https://dl.google.com/go/go$GO_VERSION.$OS-$ARCH.tar.gz
+sudo tar -C /usr/local -xzvf go$GO_VERSION.$OS-$ARCH.tar.gz
+PATH="/usr/local/go/bin:${PATH}:${GOPATH}/bin"
 echo "export GOPATH=$(pwd)/go" >> $(eval echo ~${USERID})/.bashrc && \
     echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> $(eval echo ~${USERID})/.bashrc
 go get -u github.com/golang/dep/cmd/dep
 
+
 # install singularity
-export SING_DIR=/usr/local
-export SING_VERSION=v3.5.2 # or another tag or branch if you like && \
 go get -d github.com/sylabs/singularity
 cd $GOPATH/src/github.com/sylabs/singularity && \
     git fetch && \
@@ -38,6 +46,9 @@ cd $GOPATH/src/github.com/sylabs/singularity && \
 ./mconfig --prefix=$SING_DIR && \
     make -C ./builddir && \
     sudo make -C ./builddir install
+
+
+# configure singularity
 sudo sed -i 's/^ *mount *home *=.*/mount home = no/g' $SING_DIR/etc/singularity/singularity.conf
 
 echo ". ${SING_DIR}/etc/bash_completion.d/singularity" >> $(eval echo ~${USERID})/.bashrc
