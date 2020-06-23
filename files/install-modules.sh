202c4f236348694cc2d2287a54b6b0daf5ee4817
Marco De La Pierre <marco.delapierre@gmail.com>
2020-06-23 13:53:54 +0800

    fix to install modules script

--- files/install-modules.sh
+++ files/install-modules.sh
@@ -9,6 +9,7 @@ cd /usr/bin
 # assuming version 8.6 here
 sudo ln -s tclsh8.6 tclsh
 
+. /etc/profile.d/modules.sh
 MODULE_VERSION=$( module -V | cut -d ' ' -f 3 )
 echo "export MODULE_VERSION=\"$MODULE_VERSION\"" >> $(eval echo ~${USERID})/.bashrc
 
