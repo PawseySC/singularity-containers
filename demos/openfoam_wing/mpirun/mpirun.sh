0eadccc78f41e7cefb21a69ec0c1f61dd99cfb05
Marco De La Pierre <marco.delapierre@gmail.com>
2021-06-16 21:55:01 +0800

    wing ex customised for this tutorial

--- demos/openfoam_wing/mpirun/mpirun.sh
+++ demos/openfoam_wing/mpirun/mpirun.sh
@@ -4,11 +4,9 @@ NTASKS="4"
 export theImage="library://marcodelapierre/beta/openfoam:v2012"
 
 # this configuration depends on the host
-export MPICH_ROOT="/opt/intel/oneapi"
-export MPICH_LIBS="$( which mpirun )"
-export MPICH_LIBS="${MPICH_LIBS%/bin/mpirun*}/lib"
+export MPICH_ROOT="/opt/mpich/mpich-3.1.4/apps"
 
 export SINGULARITY_BINDPATH="$MPICH_ROOT"
-export SINGULARITYENV_LD_LIBRARY_PATH="$MPICH_LIBS:$MPICH_LIBS/libfabric:\$LD_LIBRARY_PATH"
+export SINGULARITYENV_LD_LIBRARY_PATH="$MPICH_ROOT/lib:\$LD_LIBRARY_PATH"
 
 ./runAll.sh
