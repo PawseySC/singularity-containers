878e5402dee525248d8a5742cf3d24c97e5700c8
Marco De La Pierre <marco.delapierre@gmail.com>
2021-11-05 13:40:16 +0800

    fix to trinity example

--- demos/trinity/create_overlay.sh
+++ demos/trinity/create_overlay.sh
@@ -4,7 +4,7 @@ export COUNT="200"
 export BS="1M"
 export FILE="my_overlay"
 singularity exec docker://ubuntu:18.04 bash -c " \
-    mkdir -p overlay_tmp/upper && \
+    mkdir -p overlay_tmp/upper overlay_tmp/work && \
     dd if=/dev/zero of=$FILE count=$COUNT bs=$BS && \
     mkfs.ext3 -d overlay_tmp $FILE && \
     rm -rf overlay_tmp \
